17281a198a914b91e8fd6d6c4e058b9a3ada8dfa
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-01-08 18:14:49 +0000

    feat: add CJS export
    previously this package only exported ESM code

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-import packageJson from './package.json' assert { type: 'json' }
 import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
@@ -6,10 +5,16 @@ import commonjs from '@rollup/plugin-commonjs'
 /** @type import('rollup') */
 export default {
   input: 'lib/index.ts',
-  output: {
-    file: packageJson.main,
-    format: 'esm',
-  },
+  output: [
+    {
+      file: 'dist/index.mjs',
+      format: 'esm',
+    },
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+    },
+  ],
   external: [],
   plugins: [
     commonjs(),
